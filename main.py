24b8f98ac548f20b84bee891ebd3f613e3e0e879
Ben McKay <33388321+Sir-Veillance@users.noreply.github.com>
2019-02-26 18:17:26 -0500

    Add printing and many bug fixes
    Program is now complete and all syntax errors/bugs have been ironed out. It is currently in a working state.

--- main.py
+++ main.py
@@ -10,29 +10,31 @@ class Node(object):
 		self.parent = parent
 		self.g = self.generate_g()
 		self.h = self.generate_h()
-		self.f = g + h
+		self.f = self.g + self.h
 
 	def generate_g(self):
 		g = 0
 		previous_node = self.parent
+		if previous_node == None:
+			return g
 		while previous_node.parent != None:
 			g += 1
 			previous_node = previous_node.parent
 		return g
 
 	def generate_h(self):
-		return = (target.y - self.y)**2 + (target.x - self.x)**2
+		return (self.target[1] - self.y)**2 + (self.target[0] - self.x)**2
 
-grid = [['oooooooooo'],
-		['oooooooooo'],
-		['oxxxxxxooo'],
-		['oooooooooo'],
-		['oooooooooo'],
-		['ooooxxxxoo'],
-		['oooooooooo'],
-		['ooxxxooooo'],
-		['oooooooooo'],
-		['oooosooooo']]
+grid = ['oooooooooo',
+		'oooooooooo',
+		'oxxxxxxooo',
+		'oooooooooo',
+		'oooooooooo',
+		'ooooxxxxoo',
+		'oooooooooo',
+		'ooxxxooooo',
+		'oooooooooo',
+		'oooosooooo']
 
 start_coordinates = (4, 9)
 
@@ -50,29 +52,48 @@ def get_path(start_node, end_coordinates, grid):
 
 	found = False
 
-	while len(open_nodes > 0) and !(found):
+	while len(open_nodes) > 0 and not found:
 		current_node = open_nodes[0]
 		for i in range(-1, 2):
 			for j in range(-1, 2):
 				if current_node.x + i < 0 or current_node.x + i > 9:
 					pass
-				elif current_node.y + j < 0 or current_node.x + j > 9:
+				elif current_node.y + j < 0 or current_node.y + j > 9:
 					pass
 				elif current_node.x + i == current_node.x and current_node.y + j == current_node.y:
 					pass
 				elif current_node.x + i == end_coordinates[0] and current_node.y + j == end_coordinates[1]:
 					path_node = Node(current_node.x + i, current_node.y + j, end_coordinates, current_node)
 					found = True
-				elif grid[current_node.x + i, current_node.y + j] != 'x':
+				elif grid[current_node.y + j][current_node.x + i] != 'x':
 					open_nodes.append(Node(current_node.x + i, current_node.y + j, end_coordinates, current_node))
 					open_nodes.sort(key=lambda x: x.f, reverse=False)
 				else:
 					pass
+		closed_nodes.append(open_nodes.pop(open_nodes.index(current_node)))
+
 
 	path = []
 	pathing_node = path_node
 	while pathing_node.parent != None:
-		path.append(pathing_node.x, pathing_node.y)
+		path.append((pathing_node.x, pathing_node.y))
 		pathing_node = pathing_node.parent
 
 	return path
+
+shortest_path = get_path(start_node, end_coordinates, grid)
+
+for i in range(10):
+	line = ''
+	for j in range(10):
+		if j == start_node.x and i == start_node.y:
+			line += 'S'
+		elif j == end_coordinates[0] and i == end_coordinates[1]:
+			line += 'T'
+		elif (j, i) in shortest_path:
+			line += 'x'
+		elif grid[i][j] == 'x':
+			line += 'w'
+		else:
+			line += '-'
+	print(line)
